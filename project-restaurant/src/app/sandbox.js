f6a36f3bd9a9ef06937fc930f51ef554299d88f7
Vanessa Dickey <vcdickey@gmail.com>
2023-10-13 08:44:06 -0400

    page works, added prettier

---
+++ project-restaurant/src/app/sandbox.js
@@ -0,0 +1,177 @@
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+const axios = require("axios");
+
+export default function Home() {
+  const [appetizers, setAppetizers] = useState([]);
+  const [breakfast, setBreakfast] = useState([]);
+  const [lunch, setLunch] = useState([]);
+  const [dinner, setDinner] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.jsonkeeper.com/b/MDXW")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        setAppetizers(response.data.appetizers);
+        setBreakfast(response.data.breakfast);
+        setLunch(response.data.lunch);
+        setDinner(response.data.dinner);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+
+  return (
+    <>
+      <header className="text-center">
+        <span className="align-middle">
+          <h1>348 E Main St, Lexington, KY</h1>
+        </span>
+        <h1>JONATHAN</h1>
+        <h1>open daily 12:31am - 1:02am</h1>
+      </header>
+
+      <div className="container">
+        <Image
+          src="/bird.png"
+          width={500}
+          height={500}
+          alt="neopets throwback photo of island bird"
+        />
+      </div>
+
+      <div className="d-flex align-items-start">
+        <div
+          className="nav flex-column nav-pills me-3"
+          id="v-pills-tab"
+          role="tablist"
+          aria-orientation="vertical"
+        >
+          <button
+            className="nav-link active"
+            id="v-pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-home"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-home"
+            aria-selected="true"
+          >
+            Home
+          </button>
+          <button
+            className="nav-link"
+            id="v-pills-profile-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-profile"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-profile"
+            aria-selected="false"
+          >
+            Profile
+          </button>
+          <button
+            className="nav-link"
+            id="v-pills-messages-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-messages"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-messages"
+            aria-selected="false"
+          >
+            Messages
+          </button>
+          <button
+            className="nav-link"
+            id="v-pills-settings-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-settings"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-settings"
+            aria-selected="false"
+          >
+            Settings
+          </button>
+        </div>
+        <div className="tab-content" id="v-pills-tabContent">
+          <div
+            className="tab-pane fade show active"
+            id="v-pills-home"
+            role="tabpanel"
+            aria-labelledby="v-pills-home-tab"
+          >
+            <h2>Appetizers</h2>
+            <ul>
+              {appetizers.map((item, index) => (
+                <li key={index}>
+                  {item.name} - {item.price}
+                </li>
+              ))}
+            </ul>
+            <h2>Breakfast</h2>
+            <ul>
+              {breakfast.map((item, index) => (
+                <li key={index}>
+                  {item.name} - {item.price}
+                </li>
+              ))}
+            </ul>
+            <h2>Lunch</h2>
+            <ul>
+              {lunch.map((item, index) => (
+                <li key={index}>
+                  {item.name} - {item.price}
+                </li>
+              ))}
+            </ul>
+            <h2>Dinner</h2>
+            <ul>
+              {dinner.map((item, index) => (
+                <li key={index}>
+                  {item.name} - {item.price}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div
+            className="tab-pane fade"
+            id="v-pills-profile"
+            role="tabpanel"
+            aria-labelledby="v-pills-profile-tab"
+          >
+            ...
+          </div>
+          <div
+            className="tab-pane fade"
+            id="v-pills-messages"
+            role="tabpanel"
+            aria-labelledby="v-pills-messages-tab"
+          >
+            ...
+          </div>
+          <div
+            className="tab-pane fade"
+            id="v-pills-settings"
+            role="tabpanel"
+            aria-labelledby="v-pills-settings-tab"
+          >
+            ...
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
